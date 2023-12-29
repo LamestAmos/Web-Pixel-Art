81a5158dab9cc36febc79e9078fc81755ef3a1d0
Sadudeen Mubarak <155117192+LamestAmos@users.noreply.github.com>
2023-12-29 17:56:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const boxInput = document.querySelector("#box-input")
 const makeBtn = document.querySelector(".make-btn")
 const playArea = document.querySelector(".play-area")
 const deleteBtn = document.querySelector(".delete-btn")
+const editBtn = document.querySelector('.edit-btn')
+const highlightedBoxes = document.getElementsByClassName('box-highlight')
 
 // *Functions
 function makeBox(color){
@@ -17,12 +19,19 @@ function makeBox(color){
 function highlightBox(box){
     box.addEventListener('click', () => {
     box.classList.toggle('box-highlight')
+    if (highlightedBoxes.length) editBtn.disabled = false
+    else editBtn.disabled = true
 }) 
 }
 
 // TODO Add more Utility
-function moveBox(){}
-function editBox(){}
+function editBox(color){
+        [...highlightedBoxes].forEach(highlightedBox => {
+            highlightedBox.style.backgroundColor = color
+            highlightedBox.classList.remove('box-highlight')
+        });
+        editBtn.disabled = true
+}
 function deleteBox(){}
 
 function deleteBoxes(){
@@ -32,3 +41,6 @@ function deleteBoxes(){
 // *Event Handlers
 makeBtn.addEventListener('click', () => {makeBox(boxInput.value)})
 deleteBtn.addEventListener('click', () => {deleteBoxes()})
+editBtn.addEventListener('click', () => {editBox(boxInput.value)})
+
+
